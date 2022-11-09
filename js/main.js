4d16275881ea5ea360fca2e06d41b74862754602
luciobo <luciobo@icloud.com>
2022-11-09 22:55:42 +0100

    sistemata function manca div html help florian!

--- js/main.js
+++ js/main.js
@@ -4,34 +4,37 @@
 
 // creo il bottone con cui genererò il click 
 
-
-
 const btnCreaGriglia = document.getElementById("btn_creagriglia");
-console.log(btnCreaGriglia)
+console.log(btnCreaGriglia);
 
 
 btnCreaGriglia.addEventListener("click", function () {
-    console.log("hai cliccato il pulsante")
+    console.log("hai cliccato il pulsante");
 
-    
+    generaLaGriglia(3);
+
 });
 
-// ora che ho reciuperato con fatica il click devop creare una 
+// ora che ho reciuperato con fatica il click devo creare una 
 // funzione che mi genri la griglia 
+
+
 
 function generaLaGriglia(celle) {
 
     // recupero il cointenitore dove mettero la mia griglia
-    const contenitoreGriglia = document.querySelector(".contenitore_griglia")
-    // ora devo creare qualcosa che mi permetta di generare la griglia sessa
+    const contenitoreGriglia = document.querySelector(".contenitore-griglia")
+    // ora devo creare qualcosa che mi permetta di generare la griglia stessa
 
     const celleTotali = celle * celle;
 
     for (let i = 0; i < celleTotali; i++) {
-        
-        const nuovaCellaCreata = document.createElement("div")
-        
-        nuovaCellaCreata.classList.add("contenuto-cella")
+
+        const nuovaCellaCreata = document.createElement("div");
+
+        nuovaCellaCreata.classList.add("contenuto-cella");
+        nuovaCellaCreata.style.width = `calc(100% / ${celle})`;
+        console.log(nuovaCellaCreata)
     }
-    contenitoreGriglia.append( nuovaCellaCreata )
+    contenitoreGriglia.append()
 }
