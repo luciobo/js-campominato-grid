d3f529658a741b0994b685a3d9d3d310448d0473
luciobo <luciobo@icloud.com>
2022-11-09 15:52:04 +0100

    inizata creazione html e js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// cosa devo fare? 
+// recuperare il click sul pulsante per genare la griglia
+
+
+// creo il bottone con cui genererò il click 
+
+const btnCreaGriglia = document.getElementById("btn_creagriglia");
+console.log(btnCreaGriglia)
+
+
+btnCreaGriglia.addEventListener("click", function () {
+    console.log(btnCreaGriglia);
+}
